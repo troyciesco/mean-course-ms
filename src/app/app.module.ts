a9880952bbe7044ef7167b5c24188e8ce851b9b6
Troy Ciesco <tmciesco@gmail.com>
2019-10-27 15:10:22 -0400

    post create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser"
+import { NgModule } from "@angular/core"
+import { FormsModule } from "@angular/forms"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
+import { PostCreateComponent } from "./posts/post-create/post-create.component"
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PostCreateComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html"
+})
+export class PostCreateComponent {
+  enteredValue = ""
+  newPost = "No Content."
+  // onAddPost(postInput: HTMLTextAreaElement) {
+  //   this.newPost = postInput.value
+  // }
+  onAddPost() {
+    this.newPost = this.enteredValue
+  }
+}
