936430087955f36899cd3955e1d75b36fa18ebaa
Troy Ciesco <tmciesco@gmail.com>
2019-10-27 15:18:17 -0400

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from "@angular/forms"
 
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
-import { PostCreateComponent } from "./posts/post-create/post-create.component"
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
