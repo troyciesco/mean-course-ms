1b4ceac927aba3f5ca6076332a046c923cccbc19
Troy Ciesco <tmciesco@gmail.com>
2019-10-27 15:26:18 -0400

    updated post-create to use material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser"
 import { NgModule } from "@angular/core"
 import { FormsModule } from "@angular/forms"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { MatInputModule, MatCardModule, MatButtonModule } from "@angular/material"
 
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
-import { PostCreateComponent } from "./posts/post-create/post-create.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { PostCreateComponent } from "./posts/post-create/post-create.component"
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
