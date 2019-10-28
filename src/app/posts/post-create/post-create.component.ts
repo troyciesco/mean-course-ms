0e860cc5ad33ac22b0bac14cdf5ba4dc04fd2682
Troy Ciesco <tmciesco@gmail.com>
2019-10-27 22:11:39 -0400

    added edit and delete button styles

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -22,5 +22,6 @@ export class PostCreateComponent {
     //const post: Post = { title: form.value.title, content: form.value.content }
     //this.postCreated.emit(post)
     this.postsService.addPost(form.value.title, form.value.content)
+    form.resetForm()
   }
 }
